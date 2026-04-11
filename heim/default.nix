c71fa1ffdea47c8cc9afdb6192135bf9bae386f4
Oscar Ahlén <github@ahlen.dev>
2026-04-11 22:30:00 +0200

    Avoid redundant rebuilds of rust project

--- heim/default.nix
+++ heim/default.nix
@@ -1,9 +1,22 @@
-{ rustPlatform }:
+{ lib, rustPlatform }:
+let
+  fs = lib.fileset;
+  sourceFiles = fs.unions [
+    ./Cargo.toml
+    ./Cargo.lock
+    (fs.fileFilter (file: file.hasExt "rs") ./src)
+  ];
+
+in
 rustPlatform.buildRustPackage {
   pname = "nix-heim";
   version = "1.0.0";
 
-  src = ./.;
+  src = fs.toSource {
+    root = ./.;
+    fileset = sourceFiles;
+  };
+
   cargoLock.lockFile = ./Cargo.lock;
 
   meta.mainProgram = "heim";
